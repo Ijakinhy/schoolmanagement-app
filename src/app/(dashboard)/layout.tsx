e5694bb4e953271b20d92a284bbd0853808c3ed4
jakin <ijakinhy@gmail.com>
2025-04-04 15:30:48 +0200

    sidebar   dashboard navbar and userCard

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,35 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className=" flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]   top-16 p-4">
+        <div className="">
+          <Link
+            href="/"
+            className="flex items-center justify-center lg:justify-start gap-2"
+          >
+            <Image src="/logo.png" alt="logo" width={32} height={32} />
+            <span className="hidden lg:block font-bold">SchooLama</span>
+          </Link>
+          <Menu />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA]  flex flex-col">
+        <div className="sticky top-0 z-50 bg-white">
+          <Navbar />
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+}
