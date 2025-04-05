0c60b7ec82f57aaa21df7fcd547e947818238435
jakin <ijakinhy@gmail.com>
2025-04-05 13:53:55 +0200

    fininishing frontend

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
-const SignInPage = () => {
+const LoginPage = () => {
   return (
-    <div>SignInPage</div>
+    <div className=''>LoginPage</div>
   )
 }
 
-export default SignInPage
+export default LoginPage
