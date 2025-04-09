b51d6cafaea35fbb6820a37378c5cb7d2ff64c0c
jakin <ijakinhy@gmail.com>
2025-04-09 22:28:00 +0200

    teacher relations

--- drizzle/schema/class/classSchema.ts
+++ drizzle/schema/class/classSchema.ts
@@ -2,6 +2,7 @@ import { relations } from "drizzle-orm"
 import { int, mysqlTable, varchar } from "drizzle-orm/mysql-core"
 import { lesson } from "../lesson/lessonSchema"
 import { student } from "../student/studentSchema"
+import { teacher } from "../teacher/TeacherSchema"
 
 
 
@@ -9,12 +10,17 @@ export const classSchema = mysqlTable("class", {
     id:int("id").primaryKey().autoincrement(),
     name: varchar("name", { length: 30 }).notNull().unique(),
     capacity: int("capacity").notNull(),
-
+    supervisorId: varchar("supervisor_id",{length:36}).references(()=> teacher.id),
 })
 
-export  const ClassRelations = relations(classSchema, ({ many }) => ({
+export  const ClassRelations = relations(classSchema, ({ many,one }) => ({
     lessons:many(lesson),
     students:many(student),
+    supervisor: one(teacher, {
+        fields: [classSchema.supervisorId],
+        references: [teacher.id]
+    })
+    
 }))
 
 export type Class = typeof classSchema.$inferSelect
--- drizzle/schema/teacher/TeacherSchema.ts
+++ drizzle/schema/teacher/TeacherSchema.ts
@@ -2,15 +2,16 @@ import { mysqlTable, varchar, timestamp, mysqlEnum } from 'drizzle-orm/mysql-cor
 import { relations } from 'drizzle-orm';
 import { teacherToSubject } from '../joins/teacherToSubject';
 import { lesson } from '../lesson/lessonSchema';
+import { classSchema } from '../class/classSchema';
 
 
 export const teacher = mysqlTable('teacher', {
   id: varchar('id', { length: 36 }).primaryKey().unique(),
   username: varchar('username', { length: 30 }).unique().notNull(),
-  surname: varchar('surname', { length: 30 }).unique().notNull(),
+  surname: varchar('surname', { length: 30 }).notNull(),
   name: varchar('name', { length: 30 }).notNull(),
-  email: varchar('email', { length: 30 }),
-  phone: varchar('phone', { length: 30 }),
+  email: varchar('email', { length: 30 }).unique(),
+  phone: varchar('phone', { length: 30 }).unique().notNull(),
   address: varchar('address', { length: 30 }).notNull(),
   bloodType: varchar('blood_type', { length: 30 }).notNull(),
   sex: mysqlEnum('sex', ['male', 'female']).notNull(),
@@ -20,4 +21,5 @@ export const teacher = mysqlTable('teacher', {
 export const TeacherRelations = relations(teacher, ({ many }) => ({
   teacherSubjects: many(teacherToSubject), 
   lessons: many(lesson),
+  supervisor: many(classSchema),
 }));
